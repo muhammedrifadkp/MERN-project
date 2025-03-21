85d1453a06c616fe24ffbca32e73848a88843c5b
rifad <muhammedrifadkp3@gmail.com>
2025-03-21 09:57:44 +0530

    connected live front end

--- back-end/app.js
+++ back-end/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://tseepacademy.vercel.app", credentials: true }));
 app.use("/uploads", express.static("uploads")); // ✅ Serve uploaded images
 
 // Routes
